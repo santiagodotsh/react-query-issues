cfd6905ed9dd456fce9c7b57d764b3e57b43f3c6
Santiago Ramos <santiagodotsh@gmail.com>
2024-10-09 12:34:41 -0500

    create update creation date

--- src/issues/components/issue-item.tsx
+++ src/issues/components/issue-item.tsx
@@ -3,6 +3,7 @@ import { useQueryClient } from '@tanstack/react-query'
 import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi'
 import { getIssue } from '../actions/get-issue'
 import { getComments } from '../actions/get-comments'
+import { timeSince } from '../../helpers/time-since'
 import { type Issue, State } from '../interfaces/issue'
 
 interface Props {
@@ -52,7 +53,7 @@ export function IssueItem({ issue }: Props) {
         </a>
 
         <span className='text-gray-500'>
-          #{issue.number} opened 2 days ago by{' '}
+          #{issue.number} opened {timeSince(issue.created_at)} ago by{' '}
           <span className='font-bold'>{issue.user.login}</span>
         </span>
 
@@ -78,7 +79,9 @@ export function IssueItem({ issue }: Props) {
       <div className='flex flex-col mx-2 items-center'>
         <FiMessageSquare size={30} className='min-w-5' color='gray' />
         
-        <span className='px-4 text-gray-400'>{issue.comments}</span>
+        <span className='px-4 text-gray-400'>
+          {issue.comments}
+        </span>
       </div>
     </div>
   )
